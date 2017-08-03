920f33dc41d2f1e35a490d4985d0d635accf21cf
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-03 20:20:40 +0100

    File Structure

---
+++ Chapter2/problems.rb
@@ -0,0 +1,10 @@
+puts '12' + 12
+puts '2' * '5'
+puts ' '
+puts 'Betty' + 12
+puts 'Fred' * 'John'
+puts ' '
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down
