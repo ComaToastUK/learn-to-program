6622e06022d712db0697d9a490f19025b92a4822
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-04 01:38:32 +0100

    Chapter 5

---
+++ Chapter4/biggerIsBetter.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favourite number?'
+favNum = Integer(gets.chomp)
+biggerNum = favNum + 1
+puts 'You should consider ' + biggerNum.to_s + ', it\'s clearly bigger and therefore better...'
---
+++ Chapter5/angryBoss.rb
@@ -0,0 +1,3 @@
+puts 'WHAT IN THE HELL DO YOU WANT?!?'
+answer = String(gets.chomp)
+puts 'WHADDAYA MEAN "' + answer.upcase + '"?!? YOU\'RE FIRED!!'
